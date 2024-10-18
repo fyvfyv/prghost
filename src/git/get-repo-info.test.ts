d3476ad5806f5414b36e860bc6b1720f3216c4bf
Oleg Petrov <petrov.oleg.s@gmail.com>
2024-10-18 17:03:21 +0100

    ✅ Update getRepoInfo tests

--- src/git/get-repo-info.test.ts
+++ src/git/get-repo-info.test.ts
@@ -12,23 +12,27 @@ import { getRepoInfo } from './get-repo-info';
 
 describe('getRepoInfo', () => {
     let mockGetRemotes: MockInstance;
+    let mockGitRaw: MockInstance;
     let mockExit: MockInstance;
-    let consoleError: MockInstance;
+    let mockConsoleError: MockInstance;
 
     beforeEach(() => {
         mockGetRemotes = vi.spyOn(SimpleGit, 'getRemotes');
         mockExit = vi
             .spyOn(process, 'exit')
             .mockImplementation(() => undefined as never);
-        consoleError = vi.spyOn(console, 'error');
+        mockConsoleError = vi.spyOn(console, 'error');
+        mockGitRaw = vi.spyOn(SimpleGit, 'raw').mockReturnValue('HEAD branch: test' as never);
     });
 
     afterEach(() => {
         mockGetRemotes.mockRestore();
         mockExit.mockRestore();
+        mockConsoleError.mockRestore();
+        mockGitRaw.mockRestore();
     });
 
-    it('should return owner and repo when origin remote is present and URL is valid', async () => {
+    it('should return owner, baseBranch and repo when origin remote is present and URL is valid', async () => {
         const remotes = [
             {
                 name: 'origin',
@@ -38,7 +42,21 @@ describe('getRepoInfo', () => {
         mockGetRemotes.mockResolvedValueOnce(remotes);
         const result = await getRepoInfo();
 
-        expect(result).toEqual({ owner: 'owner', repo: 'repo' });
+        expect(result).toEqual({ owner: 'owner', repo: 'repo', baseBranch: 'test' });
+    });
+
+    it('Should return default baseBranch as «main» when HEAD branch is not found', async () => {
+        const remotes = [
+            {
+                name: 'origin',
+                refs: { fetch: 'git@github.com:owner/repo.git' },
+            },
+        ];
+        mockGetRemotes.mockResolvedValueOnce(remotes);
+        mockGitRaw.mockReturnValueOnce('' as never);
+        const result = await getRepoInfo();
+
+        expect(result).toEqual({ owner: 'owner', repo: 'repo', baseBranch: 'main' });
     });
 
     it('should throw an error if no origin remote is found', async () => {
@@ -52,7 +70,7 @@ describe('getRepoInfo', () => {
         const result = await getRepoInfo();
 
         expect(result).toBeUndefined();
-        expect(consoleError).toHaveBeenCalledWith(
+        expect(mockConsoleError).toHaveBeenCalledWith(
             'Error: ',
             new Error('No remote named origin found'),
         );
@@ -70,7 +88,7 @@ describe('getRepoInfo', () => {
         const result = await getRepoInfo();
 
         expect(result).toBeUndefined();
-        expect(consoleError).toHaveBeenCalledWith(
+        expect(mockConsoleError).toHaveBeenCalledWith(
             'Error: ',
             new Error('Failed to parse repository name and owner from URL'),
         );
@@ -82,7 +100,7 @@ describe('getRepoInfo', () => {
         const result = await getRepoInfo();
 
         expect(result).toBeUndefined();
-        expect(consoleError).toHaveBeenCalledWith(
+        expect(mockConsoleError).toHaveBeenCalledWith(
             'Error: ',
             new Error('getRemotes error'),
         );
