571299b83f2dfa3496389ed63146fd13ef4915da
Oleg Petrov <petrov.oleg.s@gmail.com>
2024-10-18 13:47:45 +0100

    🎉 Init base structure
    Init project: added base scripts and configs

---
+++ esbuild.config.js
@@ -0,0 +1,15 @@
+import esbuild from 'esbuild';
+
+// Configuration for esbuild
+esbuild
+    .build({
+        entryPoints: ['src/index.ts'],
+        bundle: true,
+        platform: 'node',
+        outfile: 'dist/index.js',
+        banner: {
+            js: '#!/usr/bin/env node',
+        },
+        sourcemap: false,
+    })
+    .catch(() => process.exit(1));
